f4e050bdc4c0ec87759343583f10fb194699710d
codeneya <abhiyaan.2030@gmail.com>
2026-06-26 12:19:31 +0530

    Initial commit with notes and scripts

---
+++ scripts/bd/birthday.py
@@ -0,0 +1,2 @@
+age= eval(input("What was your age on 1st January 2026? : "))
+print (f"\033[43m"f"Today is July 3rd, which is your birthday\nSo that means that today you are {age+1} years old (don't think I don't see those wrinkles)\nI'm so lucky to have you to look up to you (and help with my homework)\nHAPPY BIRTHDAY!!! :)\nFrom your little sister\n❤️" "\033[0m")
